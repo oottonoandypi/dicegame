5e7779394d06da3ceb0bdde0b59e3a7c255ef9d4
yunyiding <dlrinny8@gmail.com>
2023-02-12 17:09:14 -0700

    Minor updates.

--- js/index.js
+++ js/index.js
@@ -7,4 +7,12 @@ window.onload=function(){
     document.querySelector("#player_one img").alt="Dice "+playerOne;
     document.querySelector("#player_two img").src="img/dice"+playerTwo+".png";
     document.querySelector("#player_two img").alt="Dice "+playerTwo;
+
+    if(playerOne==playerTwo){
+        document.querySelector("#result h1").textContent="Tie!";
+    }else if(playerOne>playerTwo){
+        document.querySelector("#result h1").textContent="Player One Won!";
+    }else{
+        document.querySelector("#result h1").textContent="Player Two Won!";
+    }
 };
